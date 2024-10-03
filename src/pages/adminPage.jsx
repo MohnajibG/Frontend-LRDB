0e5f3645d1d3648ff476be3836408c48fe22780f
MNG <guerchaoui7@hotmail.com>
2024-10-03 23:18:05 +0200

    route put added

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -9,13 +9,45 @@ import "../assets/styles/pageAdmin.css";
 const AdminPage = () => {
   const [dataOrder, setDataOrder] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [deleteData, setDeleteData] = useState("");
+  const [changeState, setChangeState] = useState("");
+
+  const handleDelete = async (orderId) => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:3000/order/${orderId}`
+      );
+      setDeleteData(response.data);
+      setDataOrder((prevOrders) =>
+        prevOrders.filter((order) => order._id !== orderId)
+      );
+    } catch (error) {
+      console.log("Erreur lors de la suppression");
+    }
+  };
+
+  const handleChangeStateOrder = async (orderId) => {
+    try {
+      const response = await axios.put(
+        `http://localhost:3000/order/${orderId}`,
+        { etat: true }
+      );
+      setChangeState(response.data);
+      setDataOrder((prevOrders) =>
+        prevOrders.map((order) =>
+          order._id === orderId ? { ...order, etat: true } : order
+        )
+      );
+    } catch (error) {
+      console.log("Erreur lors du changement de statut");
+    }
+  };
 
   useEffect(() => {
     const fetchDataOrder = async () => {
       try {
         const response = await axios.get("http://localhost:3000/orders");
         setDataOrder(response.data);
-        console.log(response.data);
         setIsLoading(false);
       } catch (error) {
         console.log("Erreur lors de la récupération des commandes ===>", error);
@@ -29,55 +61,57 @@ const AdminPage = () => {
     <div>Loading...</div>
   ) : (
     <main className="admin-page">
-      {dataOrder.map((order) => {
-        return (
-          <div key={order._id} className="commande">
-            <div className="header-commande">
-              <button className="button-check">
-                <FaCheck />
-              </button>
-              <button className="button-delete">
-                <IoMdClose />
-              </button>
-              <h3>Commande #{order.orderNumber}</h3>
-              <p> {new Date(order.createdAt).toLocaleString()}</p>
-              {order.etat === false ? (
-                <div
-                  className="etat-order"
-                  style={{
-                    backgroundColor: "red",
-                    width: "30px",
-                    height: "30px",
-                    borderRadius: "50%",
-                  }}
-                ></div>
-              ) : (
-                <div
-                  className="etat-order"
-                  style={{
-                    backgroundColor: "green",
-                    width: "30px",
-                    height: "30px",
-                    borderRadius: "50%",
-                  }}
-                ></div>
-              )}
-            </div>
-            <h4>Détails de la commande:</h4>
-
-            {order.items.map((item) => (
-              <div key={item.name} className="commande-details">
-                <p>{item.name}</p>
-                <p>{item.quantity}</p>x<p>{item.price}€</p>
-              </div>
-            ))}
-
-            <p className="total-commande">
-              Total: {order.totalPrice?.toFixed(2)} €
-            </p>
+      {dataOrder.map((order) => (
+        <div key={order._id} className="commande">
+          <div className="header-commande">
+            <button
+              className="button-check"
+              onClick={() => handleChangeStateOrder(order._id)}
+            >
+              <FaCheck />
+            </button>
+            <button
+              className="button-delete"
+              onClick={() => handleDelete(order._id)}
+            >
+              <IoMdClose />
+            </button>
+            <h3>Commande #{order.orderNumber}</h3>
+            <p>{new Date(order.createdAt).toLocaleString()}</p>
+            {order.etat === false ? (
+              <div
+                className="etat-order"
+                style={{
+                  backgroundColor: "red",
+                  width: "30px",
+                  height: "30px",
+                  borderRadius: "50%",
+                }}
+              ></div>
+            ) : (
+              <div
+                className="etat-order"
+                style={{
+                  backgroundColor: "green",
+                  width: "30px",
+                  height: "30px",
+                  borderRadius: "50%",
+                }}
+              ></div>
+            )}
           </div>
-        );
-      })}
+          <h4>Détails de la commande:</h4>
+          {order.items.map((item) => (
+            <div key={item.name} className="commande-details">
+              <p>{item.name}</p>
+              <p>{item.quantity}</p>x<p>{item.price}€</p>
+            </div>
+          ))}
+          <p className="total-commande">
+            Total: {order.totalPrice?.toFixed(2)} €
+          </p>
+        </div>
+      ))}
     </main>
   );
 };
